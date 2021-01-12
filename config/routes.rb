620c60a6ea607105fc5997cb9aa8455106f1d80a
久保田悦也 <instrctr.etsuya@gmail.com>
2021-01-12 13:27:17 +0900

    コメント機能の作成

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,7 @@ Rails.application.routes.draw do
 
   devise_for :users
   root to: "posts#index"
-  resources :posts, only: [:index, :new, :create]
+  resources :posts, only: [:index, :new, :create] do
+    resources :comments, only: :create
+  end
 end
