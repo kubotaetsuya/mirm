c737599e37fbcedf1ca50dfac346cb1b3498a53d
久保田悦也 <instrctr.etsuya@gmail.com>
2021-01-26 12:12:27 +0900

    user編集ボタン配置

--- config/routes.rb
+++ config/routes.rb
@@ -7,5 +7,5 @@ Rails.application.routes.draw do
   resources :posts do
     resources :comments, only: [:create, :destroy]
   end
-  resources :users, only: [:show, :update, :edit]
+  resources :users, only: [:show, :update, :edit, :destroy]
 end
