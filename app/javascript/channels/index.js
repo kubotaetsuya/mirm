96007d58f41ef01c72e70ad1105be27ba2e437db
久保田悦也 <instrctr.etsuya@gmail.com>
2021-02-10 16:02:48 +0900

    jsファイルの適切な振り分け

--- app/javascript/channels/index.js
+++ app/javascript/channels/index.js
@@ -3,30 +3,3 @@
 
 const channels = require.context('.', true, /_channel\.js$/)
 channels.keys().forEach(channels)
-
-function pullDown() {
-
-  const pullDownButton = document.getElementById("lists")
-  const pullDownParents = document.getElementById("pull-down")
-  
-    pullDownButton.addEventListener('mouseover', function(){
-      this.setAttribute("style", "background-color:#FFBEDA;")
-    })
-  
-    pullDownButton.addEventListener('mouseout', function(){
-      this.removeAttribute("style", "background-color:#FFBEDA;")
-    })
-  
-    pullDownButton.addEventListener('click', function() {
-      // プルダウンメニューの表示と非表示の設定
-      if (pullDownParents.getAttribute("style") == "display:block;") {
-        // pullDownParentsにdisplay:block;が付与されている場合（つまり表示されている時）実行される
-        pullDownParents.removeAttribute("style", "display:block;")
-      } else {
-        // pullDownParentsにdisplay:block;が付与されていない場合（つまり非表示の時）実行される
-        pullDownParents.setAttribute("style", "display:block;")
-      }
-    })
-  }
-
-window.addEventListener('load', pullDown)
